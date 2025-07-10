7ef938ebed6e0eb0a7904c714db11fe14c5575b7
Darin Boyes <darin.boyes@gmail.com>
2025-07-10 10:38:18 -0400

    cleaned up device selection control flow

--- src/model/utils.py
+++ src/model/utils.py
@@ -5,14 +5,17 @@ import numpy as np
 import pandas as pd
 import os
 
-gpu_count = int(os.environ['CUDA_VISIBLE_DEVICES'].split(",")[-1])
+gpu_count = int(os.environ['CUDA_VISIBLE_DEVICES'].split(",")[-1]) + 1
+
 if gpu_count > 1:
     local_rank = int(os.environ["LOCAL_RANK"])
     os.environ['CUDA_VISIBLE_DEVICES'] = str(local_rank)
     rank = int(os.environ["RANK"])
     torch.cuda.set_device(rank)
     device=rank
+elif gpu_count == 1:
+    device = "cuda"  
 else:
-    device = "cuda" if torch.cuda.is_available() else "cpu"
+    device = "cpu"
 
 print(f"Using {device}")
